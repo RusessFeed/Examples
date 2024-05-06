0c0d89ecbe59db681ab9346bf58057c9dfde8188
Ruslan <ruslanmarlinskiy@gmail.com>
2024-05-06 22:25:16 +0300

    Chapter 1

---
+++ Example2.java
@@ -0,0 +1,16 @@
+public class Example2 {
+    public static void main(String[] args) {
+        float floatNumber = 18.3333333333333333f;
+        double doubleNumber = 18.3333333333333333;
+        int myVar1, myVar2;
+        System.out.println("Float number: " + floatNumber);
+        System.out.println("Double number: " + doubleNumber);
+
+        myVar1 = 1024;
+        System.out.println("myVar1 содержит: " + myVar1);
+        myVar2 = myVar1 / 2;
+        System.out.print("myVar2 содержит результат myVar 1 деленное на 2 = ");
+        System.out.println(myVar2);
+
+    }
+}
